7691dcd86cd70b91bb594094ce8695a88f55b5fa
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2026-07-16 18:28:04 +0100

    add one exercise for chapter 2

---
+++ 02-basics/01-printf/__init__.py
@@ -0,0 +1,19 @@
+import check50
+import check50.c
+import re
+
+regex = re.compile("   @@@@@@[ ]*\n @@      @@[ ]*\n@[ ]*\n@[ ]*\n@[ ]*\n@[ ]*\n@[ ]*\n @@      @@[ ]*\n   @@@@@@[ ]*")
+
+@check50.check()
+def exists():
+    check50.exists("printf.c")
+
+@check50.check(exists)
+def compiles():
+    check50.c.compile("printf.c", cc="gcc")
+
+@check50.check(compiles)
+def large_c_printed():
+    out = check50.run("./printf").stdout()
+    if not regex.match(out):
+        raise check50.Failure("Incorrect output")
