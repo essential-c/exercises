cffff92dfe4a90e7d95809f5137d1561d4ef72d3
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2026-07-17 11:32:43 +0100

    chapter 02 exercise 05

---
+++ 02-basics/05-types/__init__.py
@@ -0,0 +1,27 @@
+import check50
+import check50.c
+import re
+
+@check50.check()
+def exists():
+    check50.exists("types.c")
+    with open("types.c") as f:
+        sources_buf = f.read()
+    return sources_buf
+
+@check50.check(exists)
+def compiles():
+    check50.c.compile("types.c", cc="gcc")
+
+@check50.check(exists)
+def check_type(sources_buf):
+    re1 = "\n\s*unsigned\s+(int\s+)?variable"
+
+    if not re.search(re1, sources_buf):
+        print(sources_buf)
+        raise check50.Failure("Could not find a declaration with the correct "
+                "type")
+
+@check50.check(compiles)
+def output_correct():
+    check50.run("./types").stdout("10").exit()
---
+++ 02-basics/05-types/types.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+
+    variable = 10;
+
+    printf("variable is %u\n", variable);
+
+    return 0;
+}
