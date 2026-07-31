58d73fa1ec284db97ec31dd3b396e349a9ac5f54
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2026-07-31 15:05:00 +0100

    add exercise 14 for chapter 03

---
+++ 03-mm-libc/14-memcpy/__init__.py
@@ -0,0 +1,38 @@
+import check50
+import check50.c
+import re
+
+@check50.check()
+def exists():
+    check50.exists("memcpy.c")
+    with open("memcpy.c") as f:
+        sources_buf = f.read()
+    return sources_buf
+
+@check50.check(exists)
+def compiles():
+    check50.c.compile("memcpy.c", cc="gcc")
+
+@check50.check(exists)
+def validate(sources_buf):
+    if "memcpy" not in sources_buf:
+        raise check50.Failure("Cannot find call to memcpy in the code")
+
+@check50.check(compiles)
+def output_correct():
+    check50.run("./memcpy 10")\
+            .stdout("array1:( [0-9]+){10}", regex=True)\
+            .stdout("array2:( [0-9]+){10}", regex=True)\
+            .exit()
+    check50.run("./memcpy 20")\
+            .stdout("array1:( [0-9]+){20}", regex=True)\
+            .stdout("array2:( [0-9]+){20}", regex=True)\
+            .exit()
+
+@check50.check(compiles)
+def valgrind_memcheck():
+    check50.c.valgrind("./memcpy 20")\
+            .stdout("array1:( [0-9]+){20}", regex=True)\
+            .stdout("array2:( [0-9]+){20}", regex=True)\
+            .exit()
+
