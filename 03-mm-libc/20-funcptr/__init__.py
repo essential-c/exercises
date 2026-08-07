639d22fd62dc6aec5ffb405c672da78b8a230da6
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2026-08-07 17:26:37 +0100

    fix chapter 3 exercise 20

--- 03-mm-libc/20-funcptr/__init__.py
+++ 03-mm-libc/20-funcptr/__init__.py
@@ -25,3 +25,25 @@ def output_correct():
             .stdout("Christopher Johnson, 35")\
             .stdout("David Brown, 42")\
             .exit()
+    
+    check50.run("./funcptr")\
+            .stdin("1")\
+            .stdout("Alice Brown, 25")\
+            .stdout("David Brown, 42")\
+            .stdout("Emma Jones, 19")\
+            .stdout("Christopher Johnson, 35")\
+            .stdout("Amy Li, 20")\
+            .stdout("John Smith, 31")\
+            .stdout("Bob Smith, 28")\
+            .exit()
+    
+    check50.run("./funcptr")\
+            .stdin("2")\
+            .stdout("Amy Li, 20")\
+            .stdout("Bob Smith, 28")\
+            .stdout("John Smith, 31")\
+            .stdout("Emma Jones, 19")\
+            .stdout("Alice Brown, 25")\
+            .stdout("David Brown, 42")\
+            .stdout("Christopher Johnson, 35")\
+            .exit()
