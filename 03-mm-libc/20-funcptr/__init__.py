76232fc8f7eb77583c734f6ab884e055b4e36ad4
Pierre Olivier <pierre.olivier@manchester.ac.uk>
2026-08-07 16:02:06 +0100

    fix chapter 03 exercise 20

---
+++ 03-mm-libc/20-funcptr/__init__.py
@@ -0,0 +1,28 @@
+import check50
+import check50.c
+import re
+
+@check50.check()
+def exists():
+    check50.exists("funcptr.c")
+    with open("funcptr.c") as f:
+        sources_buf = f.read()
+    return sources_buf
+
+@check50.check(exists)
+def compiles():
+    check50.c.compile("funcptr.c", cc="gcc")
+
+@check50.check(compiles)
+def output_correct():
+    check50.run("./funcptr")\
+            .stdout("sort operation to apply? (0: by age, 1: by last name, 2: by total name length)")\
+            .stdin("0")\
+            .stdout("Emma Jones, 19")\
+            .stdout("Amy Li, 20")\
+            .stdout("Alice Brown, 25")\
+            .stdout("Bob Smith, 28")\
+            .stdout("John Smith, 31")\
+            .stdout("Christopher Johnson, 35")\
+            .stdout("David Brown, 42")\
+            .exit()
